db4d1555bbfd3c2330462c09291256aee84b6781
らむね <ramune.remonedo@gmail.com>
2022-09-20 20:51:24 +0900

    修正 - 機能追加

--- src/main/java/tokyo/ramune/farmmc/bossbar/FarmBossBar.java
+++ src/main/java/tokyo/ramune/farmmc/bossbar/FarmBossBar.java
@@ -1,24 +1,38 @@
 package tokyo.ramune.farmmc.bossbar;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+
 public interface FarmBossBar {
-    BossBar getBossBar();
-    Player getPlayer();
-    NamespacedKey getNamespacedKey();
+    default BossBar getBossBar() {
+        if (Bukkit.getBossBar(getNamespacedKey()) == null)
+            create();
+
+        return Bukkit.getBossBar(getNamespacedKey());
+    }
+    @Nonnull FarmBossBarType getType();
+    @Nonnull BarColor getBarColor();
+    @Nonnull BarStyle getBarStyle();
+    @Nonnull String getTitle();
+    @Nonnull Player getPlayer();
+    @Nonnull NamespacedKey getNamespacedKey();
     boolean isAutoHide();
     void setAutoHide(boolean autoHide);
     void initialize();
     void update();
 
     default void create() {
-        BossBarHandler.create(this);
+        FarmBossBarHandler.create(this);
     }
 
     default void remove() {
-        BossBarHandler.remove(this);
+        FarmBossBarHandler.remove(this);
     }
 }
