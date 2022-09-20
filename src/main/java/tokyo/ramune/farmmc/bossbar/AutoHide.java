a269a80a120bacf3cd174d8922803529b99bde39
らむね <ramune.remonedo@gmail.com>
2022-09-20 21:32:02 +0900

    追加 - AutoHide

---
+++ src/main/java/tokyo/ramune/farmmc/bossbar/AutoHide.java
@@ -0,0 +1,37 @@
+package tokyo.ramune.farmmc.bossbar;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+import tokyo.ramune.farmmc.FarmMC;
+
+import javax.annotation.Nonnull;
+
+public class AutoHide {
+    private final FarmBossBar farmBossBar;
+    private boolean isAutoHide = false;
+    private BukkitTask autoHideTask;
+
+    public AutoHide(@Nonnull FarmBossBar farmBossBar) {
+        this.farmBossBar = farmBossBar;
+    }
+
+    public boolean isAutoHide() {
+        return isAutoHide;
+    }
+
+    public void setAutoHide(boolean autoHide) {
+        isAutoHide = autoHide;
+    }
+
+    public void update() {
+        if (!isAutoHide)
+            return;
+
+        if (autoHideTask != null)
+            autoHideTask.cancel();
+
+        autoHideTask = Bukkit.getScheduler().runTaskLaterAsynchronously(FarmMC.getPlugin(), () -> {
+            if (isAutoHide) farmBossBar.getBossBar().setVisible(false);
+        }, 60);
+    }
+}
