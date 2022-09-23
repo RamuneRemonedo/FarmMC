e1b7bc874d5c88fbf9dc6fb0ed76cfb0e4439957
らむね <ramune.remonedo@gmail.com>
2022-09-23 10:59:53 +0900

    追加 - プレイヤーがログイン時のイベントリスナーの追加

---
+++ src/main/java/tokyo/ramune/farmmc/listener/PlayerLoginListener.java
@@ -0,0 +1,42 @@
+package tokyo.ramune.farmmc.listener;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import tokyo.ramune.farmmc.FarmMC;
+import tokyo.ramune.farmmc.utility.FarmPermission;
+import tokyo.ramune.farmmc.utility.PluginStatus;
+
+public class PlayerLoginListener implements Listener {
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+
+        if (FarmMC.getStatus().equals(PluginStatus.EDIT_TEMPLATE)) {
+            event.setResult(
+                    player.hasPermission(FarmPermission.JOIN_TEMPLATE.toPermission()) ?
+                            PlayerLoginEvent.Result.ALLOWED : PlayerLoginEvent.Result.KICK_OTHER
+                    );
+            event.kickMessage(Component.text(
+                    ChatColor.RED + "\nThis server is running under edit template mode." +
+                    ChatColor.AQUA + "\ndetails: Require permission - " + FarmPermission.JOIN_TEMPLATE.toPermission().getName())
+            );
+        }
+        if (FarmMC.getStatus().equals(PluginStatus.MAINTENANCE)) {
+            System.out.println(player.hasPermission(
+                    FarmPermission.JOIN_MAINTENANCE.toPermission()) ?
+                    PlayerLoginEvent.Result.ALLOWED : PlayerLoginEvent.Result.KICK_OTHER);
+            event.setResult(player.hasPermission(
+                    FarmPermission.JOIN_MAINTENANCE.toPermission()) ?
+                    PlayerLoginEvent.Result.ALLOWED : PlayerLoginEvent.Result.KICK_OTHER
+            );
+            event.kickMessage(Component.text(
+                    ChatColor.RED + "\nThis server is running under maintenance mode." +
+                            ChatColor.AQUA + "\ndetails: Require permission - " + FarmPermission.JOIN_MAINTENANCE.toPermission().getName())
+            );
+        }
+    }
+}
