d753dadac18b62220690b10ebe22cb2ef28cd428
らむね <ramune.remonedo@gmail.com>
2022-09-23 11:00:18 +0900

    変更 - プレイヤーが参加時のワールドを動的

--- src/main/java/tokyo/ramune/farmmc/listener/PlayerJoinListener.java
+++ src/main/java/tokyo/ramune/farmmc/listener/PlayerJoinListener.java
@@ -2,12 +2,15 @@ package tokyo.ramune.farmmc.listener;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import tokyo.ramune.farmmc.FarmMC;
 import tokyo.ramune.farmmc.player.PlayerStatus;
+import tokyo.ramune.farmmc.utility.PluginStatus;
+import tokyo.ramune.farmmc.world.WorldHandler;
 
 public class PlayerJoinListener implements Listener {
     @EventHandler
@@ -23,6 +26,10 @@ public class PlayerJoinListener implements Listener {
         status.updateName();
 
         // Teleport to spawn
+        Location spawnLocation = FarmMC.getConfigValue().WORLD_SPAWN_LOCATION;
+        if (FarmMC.getStatus().equals(PluginStatus.EDIT_TEMPLATE))
+            spawnLocation.setWorld(WorldHandler.getTemplateWorld());
+
         player.teleport(FarmMC.getConfigValue().WORLD_SPAWN_LOCATION);
     }
 }
