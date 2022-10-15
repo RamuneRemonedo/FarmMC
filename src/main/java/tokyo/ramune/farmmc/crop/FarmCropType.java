87751f2b06b71faea0b09039f93f7153c9e35ddf
ramune <ramune.remonedo@gmail.com>
2022-10-16 07:39:03 +0900

    Clean lines & Add

--- src/main/java/tokyo/ramune/farmmc/crop/FarmCropType.java
+++ src/main/java/tokyo/ramune/farmmc/crop/FarmCropType.java
@@ -4,107 +4,50 @@ import com.google.common.collect.Range;
 import org.bukkit.Material;
 import org.bukkit.World;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public enum FarmCropType {
-    GRASS(
-            List.of(Material.GRASS, Material.TALL_GRASS),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.2)
-    ),
-    WHEAT(
-            List.of(Material.WHEAT_SEEDS),
-            World.Environment.NORMAL,
-            Range.closed(0.5, 0.8)
-    ),
-    BEETROOT(
-            List.of(Material.BEETROOT, Material.BEETROOTS),
-            World.Environment.NORMAL,
-            Range.closed(-0.5, 0.2)
-    ),
-    CARROT(
-            List.of(Material.CARROT, Material.CARROTS),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.0)
-    ),
-    POTATO(
-            List.of(Material.POTATO, Material.POTATOES, Material.POISONOUS_POTATO),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.0)
-    ),
-    MELON(
-            List.of(Material.MELON, Material.MELON_SEEDS, Material.MELON_STEM),
-            World.Environment.NORMAL,
-            Range.closed(0.9, 1.2)
-    ),
-    PUMPKIN(
-            List.of(Material.PUMPKIN, Material.PUMPKIN_SEEDS, Material.PUMPKIN_STEM),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.0)
-    ),
-    BAMBOO(
-            List.of(Material.BAMBOO, Material.BAMBOO_SAPLING),
-            World.Environment.NORMAL,
-            Range.closed(0.8, 0.9)
-    ),
-    COCOA_BEAN(
-            List.of(Material.COCOA, Material.COCOA_BEANS),
-            World.Environment.NORMAL,
-            Range.closed(0.9, 0.9)
-    ),
-    SUGAR_CANE(
-            List.of(Material.SUGAR_CANE),
-            World.Environment.NORMAL,
-            Range.closed(0.8, 1.2)
-    ),
-    SWEET_BERRY(
-            List.of(Material.SWEET_BERRIES, Material.SWEET_BERRY_BUSH),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 0.3)
-    ),
-    CACTUS(
-            List.of(Material.CACTUS),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.2)
-    ),
-    MUSHROOM(
-            List.of(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM),
-            World.Environment.NORMAL,
-            Range.closed(0.2, 1.2)
-    ),
-    KELP(
-            List.of(Material.KELP, Material.KELP_PLANT),
-            World.Environment.NORMAL,
-            Range.closed(0.5, 0.5)
-    ),
-    SEA_PICKLE(
-            List.of(Material.SEA_PICKLE),
-            World.Environment.NORMAL,
-            Range.closed(0.5, 0.5)
-    ),
-    NETHER_WART(
-            List.of(Material.NETHER_WART),
-            World.Environment.NETHER,
-            Range.closed(2.0, 2.0)
-    ),
-    CHORUS_FRUIT(
-            List.of(Material.CHORUS_FRUIT, Material.CHORUS_FLOWER, Material.CHORUS_PLANT),
-            World.Environment.THE_END,
-            Range.closed(0.5, 0.5)
-    );
+    GRASS(World.Environment.NORMAL, Range.closed(0.2, 1.2), List.of(Material.GRASS, Material.TALL_GRASS)),
+    WHEAT(World.Environment.NORMAL, Range.closed(0.5, 0.8), List.of(Material.WHEAT)),
+    BEETROOT(World.Environment.NORMAL, Range.closed(-0.5, 0.2), List.of(Material.BEETROOTS)),
+    CARROT(World.Environment.NORMAL, Range.closed(0.2, 1.0), List.of(Material.CARROTS)),
+    POTATO(World.Environment.NORMAL, Range.closed(0.2, 1.0), List.of(Material.POTATOES)),
+    MELON(World.Environment.NORMAL, Range.closed(0.9, 1.2), List.of(Material.MELON)),
+    PUMPKIN(World.Environment.NORMAL, Range.closed(0.2, 1.0), List.of(Material.PUMPKIN)),
+    BAMBOO(World.Environment.NORMAL, Range.closed(0.8, 0.9), List.of(Material.BAMBOO, Material.BAMBOO_SAPLING)),
+    COCOA_BEAN(World.Environment.NORMAL, Range.closed(0.9, 0.9), List.of(Material.COCOA, Material.COCOA_BEANS)),
+    SUGAR_CANE(World.Environment.NORMAL, Range.closed(0.8, 1.2), List.of(Material.SUGAR_CANE)),
+    SWEET_BERRY(World.Environment.NORMAL, Range.closed(0.2, 0.3), List.of(Material.SWEET_BERRIES, Material.SWEET_BERRY_BUSH)),
+    CACTUS(World.Environment.NORMAL, Range.closed(0.2, 1.2), List.of(Material.CACTUS)),
+    MUSHROOM(World.Environment.NORMAL, Range.closed(0.2, 1.2), List.of(Material.BROWN_MUSHROOM, Material.RED_MUSHROOM)),
+    KELP(World.Environment.NORMAL, Range.closed(0.5, 0.5), List.of(Material.KELP)),
+    SEA_PICKLE(World.Environment.NORMAL, Range.closed(0.5, 0.5), List.of(Material.SEA_PICKLE)),
+    NETHER_WART(World.Environment.NETHER, Range.closed(2.0, 2.0), List.of(Material.NETHER_WART)),
+    CHORUS_FRUIT(World.Environment.THE_END, Range.closed(0.5, 0.5), List.of(Material.CHORUS_FLOWER, Material.CHORUS_PLANT));
 
     private final List<Material> cropTypes;
     private final World.Environment growEnvironment;
     private final Range<Double> temperatureRange;
 
-    FarmCropType(List<Material> cropTypes, World.Environment growEnvironment, Range<Double> temperatureRange) {
-        this.cropTypes = cropTypes;
+    FarmCropType(World.Environment growEnvironment, Range<Double> temperatureRange, List<Material> cropTypes) {
         this.growEnvironment = growEnvironment;
         this.temperatureRange = temperatureRange;
+        this.cropTypes = cropTypes;
     }
 
     public List<Material> getCropTypes() {
         return cropTypes;
+    }
+
+    @Nullable
+    public FarmCropType getCropType(Material material) {
+        for (FarmCropType value : FarmCropType.values()) {
+            if (value.getCropTypes().contains(material))
+                return value;
+        }
+
+        return null;
     }
 
     public World.Environment getGrowEnvironment() {
