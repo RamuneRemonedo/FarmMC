258377d2c9abd147bb49e1d32c904e6ee5872c30
ramune <ramune.remonedo@gmail.com>
2022-12-21 21:34:38 +0900

    バグ修正

--- src/main/java/tokyo/ramune/farmmc/core/listener/player/PlayerSwapHandItemsListener.java
+++ src/main/java/tokyo/ramune/farmmc/core/listener/player/PlayerSwapHandItemsListener.java
@@ -1,6 +1,7 @@
 package tokyo.ramune.farmmc.core.listener.player;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,6 +13,12 @@ public class PlayerSwapHandItemsListener implements Listener {
     public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event) {
         Player player = event.getPlayer();
         Location location = player.getLocation();
+
+        Material main = event.getMainHandItem() == null ? Material.AIR : event.getMainHandItem().getType();
+        Material off = event.getOffHandItem() == null ? Material.AIR : event.getOffHandItem().getType();
+
+        if (main.equals(Material.AIR) && off.equals(Material.AIR))
+            return;
 
         player.playSound(location, Sound.ENTITY_BAT_TAKEOFF, 0.5F, 1F);
     }
